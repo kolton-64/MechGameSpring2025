457ff383fe271022939564b5d27b1715c2a78ab1
Kougarz <122490236+Kougarz@users.noreply.github.com>
2025-01-24 14:48:02 -0800

    Create enemyai.py
    Start of my file that will hold the decision making for the enemy actions.

---
+++ enemyai.py
@@ -0,0 +1,10 @@
+#Files for functions, classes, and methods used by the enemy for
+#decision making.
+#################################80CHAR#########################################
+
+class DecisionMaker:
+	#set up variables
+	def __init__(self):
+		#set up constructure for DecisionMaker
+	def DoAnAction():
+		#do something	
