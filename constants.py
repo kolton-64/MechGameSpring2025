0fab0357de299ec8ec98df9bec202fa5e0b3ecc1
koltonR <koltonreimann@gmail.com>
2025-02-20 16:54:44 -0800

    added the grid system with some movement for arrow keys and clicking. Also added a file to gitignore and a method to the player grid file

---
+++ constants.py
@@ -0,0 +1,13 @@
+# Screen dimensions
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# Grid settings
+GRID_ROWS = 3
+GRID_COLS = 3
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0,255,0)
--- playerGrid.py
+++ playerGrid.py
@@ -8,3 +8,11 @@ class PlayerGrid:
     def get_player_position(self):
         #Returns the current player position as (row, col)
         return self.player_position
+    def place_player(self, new_position):
+        """Updates the grid with the new player position."""
+        old_row, old_col = self.player_position
+        self.grid[old_row][old_col] = 0  # Clear old position
+
+        new_row, new_col = new_position
+        self.grid[new_row][new_col] = 1  # Set new position
+        self.player_position = new_position
