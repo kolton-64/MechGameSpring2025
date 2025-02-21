a0d493aad53fb7cace1c205866b7373d4166cb8d
Kougarz <122490236+Kougarz@users.noreply.github.com>
2025-02-21 11:58:40 -0800

    beef BadMech
    I made sure that world StateModel was more consistent with the instantiations it is making. I also beefed up BadMech to provide more control over the bad mechs that my decision make will be handling.

--- enemyai.py
+++ enemyai.py
@@ -8,17 +8,67 @@ import playerGrid
 import weapons
 
 class BadMech(Dreadnought):
-	HealthPoints
-	leftWeapon
-	rightWeapon
-	defend
-	def __init__(self, hp, mainhand, offhand):
-		self.HealthPoints = hp
-		self.leftWeapon = new Weapon(mainhand)
-		self.rightWeapon = new Weapon(offhand)
-		self.defend = False
-	def Level_Up(level):
-		self.HealthPoints = self.HealthPoints * level
+	mechStatus
+	availableStatus
+	mechLevel
+	def __init__(self, name, weapOne, weapTwo):
+                super().__init__(
+                	weapOne.name + " & " + weapTwo.name + " Mech",
+                	1000,
+                	weapOne,
+                	weapTwo,
+                	)
+                mechStatus = "normal"
+                availableStatus = [
+                	"vulnerable",
+                	"protected",
+                	"strong",
+                	"weak",
+                	"repairing",
+                ]
+                mechLevel = 1
+	def Level_Up(self, level):
+		if level in range(1,3):
+			self.HealthPoints = self.HealthPoints * level
+			self.mechLevel = level
+		else:
+			level = (self.mechLevel)+1
+			self.HealthPoints = self.HealthPoints * level
+			self.mechLevel = level
+	def Weapon_Change(self, hand):
+		if hand:
+			self.leftWeapon = new Random_Weapon()
+		else:
+			self.rightWeapon = new Random_Weapon()
+	def Take_Damage(self, dmg):
+		if self.mechStatus == "vulnerable":
+			self.HealthPoints -= (2*dmg)
+		elif self.mechStatus == "protected":
+			self.HealthPoints -= (.5*dmg)
+		else:
+			self.HealthPoints -= dmg
+	def Heal(self, life):
+		if self.mechStatus == "repairing":
+			self.HealthPoints += (2*life)
+		else:
+			self.HealthPoints += life
+	def Change_Status(self, status):
+		if status.lower() in self.availableStatus:
+			self.mechStatus = status
+		else:
+			self.mechStatus = "normal"
+
+
+def Random_Weapon():
+	choice = random.randint(1, 4)
+	if choice == 1:
+		return new Bolter()
+	elif choice == 2:
+		return new MeleeWeapon1()
+	elif choice == 3:
+		return new AOEWeapon1()
+	else:
+		return new AssaultCannon()
 
 class StateModel:
 	playerMech
@@ -29,32 +79,37 @@ class StateModel:
 	percent_to_defend
 	def __init__(self):
 		#construct player and 9 enemies
-		self.playerMech = new Dreadnought()
+		self.playerMech = new Dreadnought(
+			"template", 1, new Bolter(), new Bolter()
+			)
 		self.playerPosition = new playerGrid()
 		self.enemyMech = []
 		self.enemyPosition = []
 		self.percent_to_move = []
 		self.percent_to_defend = []
 		for i in range(9):
-			self.enemyMech.append(new BadMech(10, sword, bolter))
+			self.enemyMech.append(
+				new BadMech(10, Random_Weapon(), Random_Weapon())
+				)
 			if 0 <= i <= 2:
 				self.enemyMech[i].Level_Up(1)
 			if 3 <= i <= 5:
 				self.enemyMech[i].Level_Up(2)
 			if 6 <= i <= 8:
 				self.enemyMech[i].Level_Up(3)
-			self.enemyMech.append(new playerGrid(3))
-			self.enemyMech.append(33)
-			self.enemyMech.append(10)
-
+			self.enemyPosition.append(new playerGrid())
+			self.percent_to_move.append(33)
+			self.percent_to_defend.append(10)
 
 class DecisionMaker:
 	#declare variables that should be initialized
-	public StateModel world_state
-	int current_enemy
-	def __init__(self, world: StateModel, difficulty: int):
+	world_state
+	current_enemy
+	game_difficulty
+	def __init__(self, world, difficulty):
 		#construct a world state
 		self.world_state = world
+		self.game_difficulty = difficulty
 	def Take_Action(self):
 		defend = self.world_state.percent_to_defend[current_enemy]
 		move = self.world_state.percent_to_move[current_enemy]
@@ -72,26 +127,26 @@ class DecisionMaker:
 			return
 		self._Attack_Action(self.weapon_secondary)
 		return
-	def _Attack_Action(weapon):
+	def _Attack_Action(self, weapon):
 		''' DONT CALL THIS FUNCTION '''
 		hero = self.world_state.playerMech
 		if ((weapon.range & hero.position) && not hero.defend):
 			hero.HealthPoints -= weapon.damage
 		hero.defend = False
-	def _Defense_Action():
+	def _Defense_Action(self):
 		''' DONT CALL THIS FUNCTION '''
 		self.world_state.enemyMech[current_enemy].defend = True
-	def _Move_Action():
+	def _Move_Action(self):
 		''' DONT CALL THIS FUNCTION '''
 		#do some inferance to figure out where to move
 		newPosition = bestmove()
 		self.world_state.enemyPosition = newPosition
-	def _Main_Is_Best():
+	def _Main_Is_Best(self):
 		''' DONT CALL THIS FUNCTION '''
 		#if main is best return true
 		enemy = self.enemyMech
 		return enemy.leftWeapon.damage >= enemy.leftWeapon.damage
-	def Change_Enemy(round, choice):
+	def Change_Enemy(self, round, choice):
 		#choice is 1-3 but based on the round will pick enemy 0-8
 		if round == 1:
 			self.current_enemy = choice - 1
