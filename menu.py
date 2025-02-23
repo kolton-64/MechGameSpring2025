72f69ce2aeb3eb714442cd84710b96531ffb83f6
James Lesperance <james.lesperance@wsu.edu>
2025-02-23 01:12:26 -0800

    Defined basic menus; Created menu UX flow

--- menu.py
+++ menu.py
@@ -7,6 +7,12 @@ class MenuType(Enum):
 	EMPTY = 0
 	MAIN = 1
 	DIFFICULTY = 2
+	IN_GAME = 3
+
+class Difficulty(Enum):
+	EASY = 0
+	MEDIUM = 1
+	HARD = 2
 
 
 class MenuController: 
@@ -14,9 +20,19 @@ class MenuController:
 		self.screen = screen
 		self.width = screen.get_width()
 		self.height = screen.get_height()
-		self.stage = 0
+
+		# stage determines if player is on main menu or in-game menu
+		self.stage: int = 0
+
 		self.menu = MenuType.EMPTY
 		self.active = False
+
+
+		# This is just a placeholder variable
+		# difficulty will be tracked in a state machine
+		# default to easy
+		self.difficulty = Difficulty.EASY
+		print(self.difficulty.value)
 
 	
 	'''
@@ -29,54 +45,94 @@ class MenuController:
 			
 	'''
 	def menuLoop(self, events):
-		if self.menu == MenuType.EMPTY:
+
+		# If theres no menu, set up the correct one
+		if self.menu == MenuType.EMPTY and self.active:
 			match self.stage:
 				case 0:
 					self.setMenu(MenuType.MAIN)
 				case 1:
-					self.setMenu(MenuType.DIFFICULTY)
+					self.setMenu(MenuType.IN_GAME)
 
-		if self.menu != MenuType.EMPTY:
+		# Only draw the menu if the menu has been properly set
+		if self.menu != MenuType.EMPTY and type(self.menu) == pygame_menu.menu.Menu:
 			self.drawMenu(events)
+
 
 	def setMenu(self, menuType):
 		print("Set menu")
 		match menuType:
 			case MenuType.MAIN:
 				self._initMainMenu()
-			case MenuType.DIFFICULTY:
-				self._initDifficultyMenu()
+			case MenuType.IN_GAME:
+				self._initInGameMenu()
 
 	def drawMenu(self, events):
+		print(type(MenuType))
 		self.menu.update(events)
-		self.menu.draw(self.screen)
+		if(type(self.menu) == pygame_menu.menu.Menu):
+			self.menu.draw(self.screen)
 
 
 	def _initMainMenu(self):
-		print("Init main menu")
+		print("Current Difficulty Level: ", self.difficulty)
 		self.menu = pygame_menu.Menu(
 			"Welcome, Pilot!",
 			self.width, self.height,
 			theme=themes.THEME_BLUE
 		)
-		self.menu.add.button("Change Difficulty", self._select_difficulty)
 		self.menu.add.button('Play', self._play)
+		self.menu.add.button("Change Difficulty", self._initDifficultyMenu)
+		self.menu.add.button("Exit", self._terminate)
+		print(" - Initialized main menu - ")
+
+
+	def _initInGameMenu(self):
+		self.menu = pygame_menu.Menu(
+			"Game Paused",
+			self.width / 2, self.height / 2,
+			theme=themes.THEME_DARK
+		)
+
+		self.menu.add.button('Resume', self._resume)
+		self.menu.add.button('Quit', self._initMainMenu)
+		print(" - Initialized in game menu - ")
 	
+	def _resume(self):
+		self.menu = MenuType.EMPTY
+
+
 	def _initDifficultyMenu(self):
 		self.menu = pygame_menu.Menu(
 			"Select Difficulty",
 			self.width, self.height,
 			theme=themes.THEME_BLUE
 		)
-		self.menu.add.button("Easy", self._play)
-		self.menu.add.button("Medium", self._play)
-		self.menu.add.button("Hard", self._play)
-		print("Init difficulty menu")
-	
-	
+		self.menu.add.button("Easy", self._setDifficultyEasy)
+		self.menu.add.button("Medium", self._setDifficultyMedium)
+		self.menu.add.button("Hard", self._setDifficultyHard)
+		self.menu.add.button("Back", self._initMainMenu)
+		print(" - Initialized difficulty menu - ")
 
-	def _select_difficulty(self):
-		print("Select difficulty")
+	
+	def _setDifficultyEasy(self):
+		self.difficulty = Difficulty.EASY
+
+	def _setDifficultyMedium(self):
+		self.difficulty = Difficulty.MEDIUM
+
+	def _setDifficultyHard(self):
+		self.difficulty = Difficulty.HARD
+
+
 
 	def _play(self):
 		print("Play")
+		pygame_menu.events.CLOSE
+		self.menu = MenuType.EMPTY
+		print(self.menu)
+		self.stage = 1
+		self.active = False
+
+	def _terminate(self):
+		pygame.quit()
