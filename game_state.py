1ec2af0fc1ec7cdcafd6006795dea29265276e42
James Lesperance <james.lesperance@wsu.edu>
2025-03-11 16:49:19 -0700

    Add unit test for currentState() function

--- game_state.py
+++ game_state.py
@@ -1,4 +1,5 @@
-import enum
+from enum import Enum
+import unittest
 
 # World state keeps track of global game state
 class GameState: 
@@ -42,16 +43,64 @@ class GameState:
 
 	def currentState(self):
 		state = None
-		if(self.menuActive and self.gameActive == False):
-			state = GameState.MAIN_MENU
-		elif(self.menuActive == False and self.gameActive):
-			state = GameState.IN_GAME
-		else:
-			state = GameState.PAUSED
+		if(self.menuActive and not self.gameActive):
+			state = State.MAIN_MENU
+		elif(not self.menuActive and self.gameActive):
+			state = State.IN_GAME
+		elif(self.menuActive and self.gameActive):
+			state = State.PAUSED
+		return state
 
 
 
-class GameState(Enum):
+class GameStateTest(unittest.TestCase):
+	def setUp(self):
+		self.GS = GameState()
+
+	'''
+		White-box unit test
+		- Unit: currentState() function
+		   - branch coverage is achieved as each branch condition is tested
+		   - statement coverage is achieved as each statement is tested
+		   - condition coverage is achieved as each condition is tested
+	'''
+	def test_currentState(self):
+		# if the menu is active and game is not active state should return MAIN_MENU
+		self.GS.setMenuActive(True)
+		self.GS.setGameActive(False)
+		state = self.GS.currentState()
+
+		self.assertEqual(state, State.MAIN_MENU)
+
+		# if the menu is not active and game is active state should return IN_GAME
+		self.GS.setMenuActive(False)
+		self.GS.setGameActive(True)
+		state = self.GS.currentState()
+
+		self.assertEqual(state, State.IN_GAME)
+
+		# if both are true, state should return PAUSED
+		self.GS.setMenuActive(True)
+		self.GS.setGameActive(True)
+		state = self.GS.currentState()
+
+		self.assertEqual(state, State.PAUSED)
+
+
+	    # inside of currentState, the only time state remains as None is when both menu and game are equal to False
+		self.GS.setMenuActive(False)
+		self.GS.setGameActive(False)
+		state = self.GS.currentState()
+
+		self.assertEqual(state, None)
+
+		
+
+
+
+
+
+class State(Enum):
 	MAIN_MENU = 0
 	IN_GAME = 1
 	PAUSED = 2 
